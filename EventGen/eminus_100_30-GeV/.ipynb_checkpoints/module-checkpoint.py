40d8332a726beaa3162a9fbe25a4318ab825e2bc
Van Tha Bik Lian <minnivan@uw.edu>
2022-09-26 15:03:23 -0400

    up to date

---
+++ EventGen/eminus_100_30-GeV/.ipynb_checkpoints/module-checkpoint.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import uproot
+
+
+
+
+
+
+def main():
+    print("hello world")
+    
+    
+if __name__ == '__main__':
+        main()
+                
+
+                
+
+                
+                
+                
+        
---
+++ EventGen/eminus_100_30-GeV/module.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import uproot
+import os
+
+
+
+
+
+
+def main():
+    print("hello world")
+    
+    
+if __name__ == '__main__':
+        main()
+                
+
+                
+
+                
+                
+                
+        
